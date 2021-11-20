22257f212c55dcd051c110c3bd0fdd2769c571d8
AlvaroCapde <acapde94@gmail.com>
2021-11-20 11:32:06 -0600

    landing

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,20 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3bede7',
+        primaryLight: '#a4ebe8',
+        primaryDark: '#329693'
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
